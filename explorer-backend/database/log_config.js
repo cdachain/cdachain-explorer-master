4ca31d35d24d841d0b8c76638950bddbfd43cdc7
zhubangbang <79228807@qq.com>
2018-08-10 13:23:08 +0800

    log config

--- explorer-backend/database/log_config.js
+++ explorer-backend/database/log_config.js
@@ -1,7 +1,7 @@
 let log4js = require('log4js');
-// let path = require('path');
+let path = require('path');
 // let fs = require('fs');
-// let basePath = path.resolve(__dirname,'./data_logs');
+let basePath = path.resolve(__dirname,'../data_logs/database');
 //确定目录是否存在，如果不存在则创建目录
 // let confirmPath = function(pathStr) {
 //     if(!fs.existsSync(pathStr)){
@@ -13,13 +13,12 @@ let log4js = require('log4js');
 // if(basePath){
 //     confirmPath(basePath);
 // }
-
 log4js.configure({
     appenders: {
         cheese: {
             category:"log",        //logger名称
             type: "dateFile",           //日志类型
-            filename: "data_logs/database",             //日志输出位置
+            filename: basePath,             //日志输出位置
             alwaysIncludePattern:true,    //是否总是有后缀名
             pattern:'-yyyy-MM-dd-hh.log'   //后缀，每小时创建一个新的日志文件
         }
